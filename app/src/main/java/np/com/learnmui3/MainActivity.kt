b6584486d91e0fbbba2ca64a5977a07f8621e913
Jagdish Ghimire <87425993+jagdish1111Ghimire@users.noreply.github.com>
2022-08-11 20:59:38 +0545

    Github has on error on my device so testing dont know which attempts works.

--- app/src/main/java/np/com/learnmui3/MainActivity.kt
+++ app/src/main/java/np/com/learnmui3/MainActivity.kt
@@ -2,13 +2,17 @@ package np.com.learnmui3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //change background color
+        elevatedButton.setBackgroundColor(ContextCompat.getColor(this,R.color.black))
     }
 }
-class IamJustCreatedToObtainOOPparadigm {
-
-}
+//class IamJustCreatedToObtainOOPparadigm {
+//
+//}
